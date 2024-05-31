282435f05c667b3f333ad72efbab434483c0a9d6
Holo Rival <martanczech@yousongs.cz>
2024-05-31 22:31:51 +0200

    oauth work in progress

--- src/views/RedirectView.vue
+++ src/views/RedirectView.vue
@@ -5,7 +5,10 @@
   </template>
   
   <script>
+import axios from 'axios';
+
   export default {
+    
     mounted() {
       const hash = window.location.hash.substr(1);
       const result = hash.split('&').reduce((res, item) => {
@@ -13,10 +16,50 @@
         res[parts[0]] = parts[1];
         return res;
       }, {});
+      console.log(result);
+
       if (result.access_token) {
-        this.$router.push({ name: 'Home' });
+        //this.$router.push({ name: 'home' });
         localStorage.setItem('access_token', result.access_token);
-      }
+        
+
+        axios.post('http://localhost:8080/ranajakub/fotionapi/', { message: result.access_token })
+        .then(response => {
+          console.log('Response:', response.data);
+        })
+        .catch(error => {
+          console.error('There was an error!', error);
+        });
+     }
+     
     }
   }
+/*
+  // insert new user to database
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function main() {
+  const newUser = await prisma.fotion_user.create({
+    data: {
+      email: 'example@example.com',
+      name: 'John Doe',
+      provider: 'GOOGLE',
+      providerId: '123456789',
+      accessToken: 'youraccesstoken',
+      refreshToken: 'yourrefreshtoken'
+    },
+  });
+
+  console.log('New user created:', newUser);
+}
+/*
+main()
+  .catch(e => {
+    console.error('Error creating user:', e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+  */
   </script>
