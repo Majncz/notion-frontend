82233d5670f71932a2ee76fe535dab9094aab1e3
Jakub Rana <kubik.rana@gmail.com>
2024-06-01 09:04:58 +0200

    sign in error handling

--- src/main.js
+++ src/main.js
@@ -15,6 +15,22 @@ app.use(pinia);
 
 axios.defaults.baseURL = useGlobalStore().apiUrl;
 axios.defaults.headers.common['user-id'] = `${useGlobalStore().userId}`;
+axios.interceptors.response.use(
+    response => {
+        // Normal response processing
+        return response;
+    },
+    error => {
+        // Check for a 401 status and specific message
+        if (error.response && error.response.status === 401 && error.response.data === "User not found") {
+            // Redirect to /signin
+            window.location.href = '/signin';
+        }
+        // Log the error and reject the promise to propagate the error
+        console.error('An error occurred:', error.message);
+        return Promise.reject(error);
+    }
+);
 
 app.config.globalProperties.mousePosition = { x: 0, y: 0 };
 
